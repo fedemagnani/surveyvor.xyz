7aab929560903bd9d79121fa472950d619fa0d1f
Filippo Carboni <56604932+filopixel@users.noreply.github.com>
2023-11-05 10:13:49 +0000

    images

--- frontend/surveyvor-app/src/router/views/HomeView.vue
+++ frontend/surveyvor-app/src/router/views/HomeView.vue
@@ -27,7 +27,7 @@
       </div>
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          <div class="hidden relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Announcing our next round of funding.
             <a href="#" class="font-semibold text-indigo-600">
               <span class="absolute inset-0" aria-hidden="true" />
@@ -37,9 +37,11 @@
           </div>
         </div>
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Surveyvor</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+            Privacy enhanced surveys for everyone.
+            <br />
+            Surveyvor is a free and open source survey platform that puts privacy first.
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <router-link
@@ -47,10 +49,6 @@
               class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Get started
             </router-link>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
-              Learn more
-              <span aria-hidden="true">→</span>
-            </a>
           </div>
         </div>
       </div>
